405b964a79f75431736dcfc183f6d11371050452
Richard Hills <richard.hills@10to8.com>
2015-12-13 22:55:30 +0000

    More deployment fixes

--- core/wsgi.py
+++ core/wsgi.py
@@ -2,10 +2,10 @@
 WSGI for pythonanywhere.com hosting
 """
 
-import sys
-project_home = u'/home/RichardHills/housing3'
-if project_home not in sys.path:
-    sys.path.append(project_home)
+#import sys
+#project_home = u'/home/RichardHills/housing3'
+#if project_home not in sys.path:
+#    sys.path.append(project_home)
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
